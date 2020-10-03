dc7b27ba5c20adaf4beeb5be22ae23930438e05a
codetiger <1272662790@qq.com>
2020-10-03 14:54:41 +0800

    优化登录与创建账户的动画

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,23 @@
                     <div class="main"></div>
                     <div class="form">
                         <h3 @click="showRegister">创建账户</h3>
-                        <div v-show="isShowRegister" class="register">
-                            <input type="text" v-model="register.username" placeholder="用户名">
-                            <input type="password" v-model="register.password" placeholder="密码">
-                            <p v-bind:class="{error:register.isError}">{{register.notice}}</p>
-                            <div class="button" @click="onRegister">创建账号</div>
-                        </div>
+                        <transition name="slide">
+                            <div v-bind:class="{show:isShowRegister}" class="register">
+                                <input type="text" v-model="register.username" placeholder="用户名">
+                                <input type="password" v-model="register.password" placeholder="密码">
+                                <p v-bind:class="{error:register.isError}">{{register.notice}}</p>
+                                <div class="button" @click="onRegister">创建账号</div>
+                            </div>
+                        </transition>
                         <h3 @click="showLogin">登录</h3>
-                        <div v-show="isShowLogin" class="login">
-                            <input type="text" v-model="login.username" placeholder="输入用户名">
-                            <input type="password" v-model="login.password" placeholder="密码">
-                            <p v-bind:class="{error:login.isError}">{{login.notice}}</p>
-                            <div class="button" @click="onLogin"> 登录</div>
-                        </div>
+                        <transition name="slide">
+                            <div v-bind:class="{show: isShowLogin}" class="login">
+                                <input type="text" v-model="login.username" placeholder="输入用户名">
+                                <input type="password" v-model="login.password" placeholder="密码">
+                                <p v-bind:class="{error:login.isError}">{{login.notice}}</p>
+                                <div class="button" @click="onLogin"> 登录</div>
+                            </div>
+                        </transition>
                     </div>
                 </div>
             </div>
@@ -49,12 +53,12 @@ export default {
     },
     methods: {
         showRegister(){
-            this.isShowRegister = true
-            this.isShowLogin = false
-        },
-        showLogin(){
             this.isShowRegister = false
             this.isShowLogin = true
+        },
+        showLogin(){
+            this.isShowRegister = true
+            this.isShowLogin = false
         },
         onRegister(){
             let result1 = this.validUsername(this.register.username)
@@ -117,7 +121,6 @@ export default {
 </script>
 
 <style lang="less">
-
 .modal-mask {
   position: fixed;
   z-index: 100;
@@ -129,12 +132,10 @@ export default {
   display: table;
   transition: opacity .3s ease;
 }
-
 .modal-wrapper {
   display: table-cell;
   vertical-align: middle;
 }
-
 .modal-container {
   width: 800px;
   height: 500px;
@@ -145,7 +146,6 @@ export default {
   transition: all .3s ease;
   font-family: Helvetica, Arial, sans-serif;
   display: flex;
-
   .main {
     flex: 1;
     background: #36bc64 url(//cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center no-repeat;
@@ -154,19 +154,18 @@ export default {
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
-
+    overflow: hidden;
     h3 {
       padding: 10px 20px;
+      margin-top: -1px;
       font-weight: normal;
       font-size: 16px;
       border-top: 1px solid #eee;
       cursor: pointer;
-
       &:nth-of-type(2){
         border-bottom: 1px solid #eee;
       }
     }
-
     .button {
       background-color: #2bb964;
       height: 36px;
@@ -178,10 +177,16 @@ export default {
       margin-top: 18px;
       cursor: pointer;
     }
-
     .login,.register {
-      padding: 10px 20px;
+      padding: 0px 20px;
       border-top: 1px solid #eee;
+       height: 0;
+       overflow: hidden;
+       transition: height 250ms;
+
+       &.show {
+        height: 193px;
+       }
 
       input {
         display: block;
